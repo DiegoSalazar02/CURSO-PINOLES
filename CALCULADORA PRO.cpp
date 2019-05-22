9335c7282d454dff84371f85aa265d4fb4323369
DiegoSalazar02 <50892111+DiegoSalazar02@users.noreply.github.com>
2019-05-21 22:51:17 -0500

    Día 1 y 2

--- CALCULADORA PRO.cpp
+++ CALCULADORA PRO.cpp
@@ -7,35 +7,35 @@ int main(){
 	int y;
 	cout<<"Dame un numero: \n";
 	cin>>x;
+	int z=x;
 	cout<<"Dame otro numero: \n";
 	cin>>y;
 	int w;
 	cout<<"Presiona el numero de acuerdo a lo que quieras: \n Suma:1\n Resta:2\n Multiplicacion:3\n Division:4\n Exponencial:5\n";
 	cin>>w;
+	while (w<1||w>5){
+	cout<<"Presiona el numero de acuerdo a lo que quieras: \n Suma:1\n Resta:2\n Multiplicacion:3\n Division:4\n Exponencial:5\n";	
+	cin>>w; //Le quito esta y pues loopea el texto de arriba 
+	}
 	switch(w){
 	case 1: 	
-	x=x+y;
-	cout<<x;
-	break;
+		x=x+y;
+		break;
 	case 2: 	
-    x=x-y;
-    cout<<x;
-    break;
+    	x=x-y;
+    	break;
     case 3:	
-    x=x*y;
-    cout<<x;
-    break;
-	case 4:	
-	cout<<x;	
-	x=x/y;
-	break;
+    	x=x*y;
+      	break;
+	case 4:		
+		x=x/y;
+		break;
 	case 5:	
-	for(int i=1;i<=y;i++){
-		x=x*x;
-	break;		
+		for(int i=1;i<=y-1;i++){
+		x=x*z;			
+		}
+		break;		
 	}
-	cout<<x;		
-	}
+	cout<<x;
 	return 0;
-	
 }
