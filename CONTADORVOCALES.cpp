536664d01431c5467efe94c72266c9c63b972332
DiegoSalazar02 <50892111+DiegoSalazar02@users.noreply.github.com>
2019-05-24 18:36:21 -0500

    Add files via upload

--- CONTADORVOCALES.cpp
+++ CONTADORVOCALES.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
 int main(){
 	char x[30];
-	getline (cin,x[29]);
 	int a=0; int e=0; int i=0; int o=0; int u=0;
 	cout<<"Dame una frase (100 palabras maximo) \n";
+	cin.getline(x,30);
 	for(int i=0;i<=30;i++){
 		switch (x[i]){
-			case a: a++;
+			case 'a': a++;
 			break;	
-			case e: e++;
+			case 'e': e++;
 			break;
-			case i: i++;
+			case 'i': i++;
 			break;
-			case o: o++;
+			case 'o': o++;
 			break;
-			case u: u++;
+			case 'u': u++;
 			break;
 		}
 		
