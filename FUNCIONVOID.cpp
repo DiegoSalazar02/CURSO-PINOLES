a7c811c28ccabdbeb43eb9f0cd2391229f577993
DiegoSalazar02 <50892111+DiegoSalazar02@users.noreply.github.com>
2019-05-30 20:14:50 -0500

    Día 6

--- FUNCIONVOID.cpp
+++ FUNCIONVOID.cpp
@@ -2,70 +2,57 @@
 
 using namespace std;
 
-int x,y,z;
-void caluculadora(int x, int y, int z){
-	int a;
+int x, y, z, w;
+void caluculadora(int a, int b, int c){
+	int w=a;
+	switch(c){
+		case 1: 
+			a=a+b;
+			cout<<a;
+			break;
+		case 2:
+			a=a-b;
+			cout<<a;
+			break;
+		case 3:
+			a=a*b;
+			cout<<a;
+			break;
+		case 4:
+			if(b==0){
+				cout<<"No se puede \n";
+			}
+			else{
+			a=a/b;
+			cout<<a;
+			}
+			break;
+		case 5:
+			if(b==0){
+				cout<<'1';
+			}
+			else{
+			for(int i=0;i<b;i++){
+				a=a*w;
+			}
+			cout<<a;
+			}
+			break;
+	}
+}
+
+int main(){	
 	cout<<"Dame un numero \n";
 	cin>>x;
-	x=a;
 	cout<<"Dame otro numero \n";
 	cin>>y;
 	cout<<"1:suma \n 2:resta \n 3:multiplicacion \n 4:division \n 5:exponencial \n";
 	cin>>z;
-	switch(z){
-		case 1: 
-			x=x+y;
-			cout<<x;
-			break;
-		case 2:
-			x=x-y;
-			cout<<x;
-			break;
-		case 3:
-			x=x*y;
-			cout<<x;
-			break;
-		case 4:
-			if(y==0){
-				cout<<"No se puede \n";
-			}
-			else{
-			x=x/y;
-			cout<<x;
-			}
-			break;
-		case 5:
-			for(int i=0;i<=y;i++){
-				x=x*a;
-			}
-			cout<<x;
-			break;
-	}
-	/*if(z==1){
-		x=x+y;
-	}
-	else if(z==2){
-		x=x-y;
-	}
-	else if(z==3){
-		x=x*y;
-	}				
- 	else if(z==4){
- 		if(y=0){
-			 cout<<"No se poji \n";
-		 }
-		else{
-			x=x/y;
-		} 
-	 
-	 }
-	 else if (z==5){
-		 for(int i=1;i<=y;i++)
-		 	x=a*x;
-	 }*/
-}
-
-int main(){
-caluculadora(x,y,z);	
+caluculadora(x,y,z);
 return 0;		
 }
+
+
+/*A la hora de que la funcion "void" no regresa ningun valor, en el proceso principal, 'x','y' y 'z'
+adquieren el valor de la RAM, por lo cual podemos hacer nuevas variables e igualarlas a 'x','y' y 'z'*/
+ 
